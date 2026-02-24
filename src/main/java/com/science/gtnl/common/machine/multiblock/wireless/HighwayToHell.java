7c59030d184dbbed6fb19b96f68e85021af8d4d1
Goblin <takanashi.rikka.skycat@gmail.com>
2026-02-24 11:24:42 +0800

    尝试修复超生高速的无线模式，为超生高速添加了配方

--- src/main/java/com/science/gtnl/common/machine/multiblock/wireless/HighwayToHell.java
+++ src/main/java/com/science/gtnl/common/machine/multiblock/wireless/HighwayToHell.java
@@ -38,6 +38,7 @@ import org.jetbrains.annotations.NotNull;
 import com.gtnewhorizon.structurelib.structure.IStructureDefinition;
 import com.gtnewhorizon.structurelib.structure.ISurvivalBuildEnvironment;
 import com.gtnewhorizon.structurelib.structure.StructureDefinition;
+import com.science.gtnl.common.machine.hatch.ParallelControllerHatch;
 import com.science.gtnl.common.machine.multiMachineBase.WirelessEnergyMultiMachineBase;
 import com.science.gtnl.common.material.GTNLRecipeMaps;
 import com.science.gtnl.utils.StructureUtils;
@@ -348,6 +349,10 @@ public class HighwayToHell extends WirelessEnergyMultiMachineBase<HighwayToHell>
     public CheckRecipeResult checkProcessing() {
         mParallelTier = 0;
         mParallelTier = Math.max(mParallelTier, getParallelTier(getControllerSlot()));
+        for (ParallelControllerHatch module : GTUtility.filterValidMTEs(mParallelControllerHatches)) {
+            mParallelTier = module.mTier;
+            break;
+        }
         costingEU = BigInteger.ZERO;
         costingEUText = ZERO_STRING;
         totalOverclockedDuration = 0;
