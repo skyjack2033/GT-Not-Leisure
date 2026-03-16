5af5539b401c477f8292405e6bb3a0150ef25ba8
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2026-03-16 17:51:58 +0800

    Update MixinMTEMultiBlockBase.java

--- src/main/java/com/science/gtnl/mixins/late/Gregtech/MixinMTEMultiBlockBase.java
+++ src/main/java/com/science/gtnl/mixins/late/Gregtech/MixinMTEMultiBlockBase.java
@@ -2,6 +2,7 @@ package com.science.gtnl.mixins.late.Gregtech;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -11,8 +12,6 @@ import com.science.gtnl.config.MainConfig;
 
 import gregtech.api.interfaces.metatileentity.IMetaTileEntity;
 import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
-import gregtech.api.metatileentity.implementations.MTEHatchInput;
-import gregtech.api.metatileentity.implementations.MTEHatchInputBus;
 import gregtech.api.metatileentity.implementations.MTEMultiBlockBase;
 import gregtech.api.recipe.RecipeMap;
 
@@ -22,28 +21,34 @@ public abstract class MixinMTEMultiBlockBase {
     @Shadow
     public abstract RecipeMap<?> getRecipeMap();
 
+    @Inject(method = "addToMachineList", at = @At(value = "HEAD"))
+    private void gtnl$HatchCheck(IGregTechTileEntity aTileEntity, int aBaseCasingIndex,
+        CallbackInfoReturnable<Boolean> cir) {
+        gtnl$setRecipeMapNameIfValid(aTileEntity);
+    }
+
     @Inject(method = "addInputBusToMachineList", at = @At(value = "HEAD"))
     private void gtnl$InputBusCheck(IGregTechTileEntity aTileEntity, int aBaseCasingIndex,
         CallbackInfoReturnable<Boolean> cir) {
-        if (!MainConfig.machine.enableHatchInterfaceTerminalEnhance) return;
-        if (aTileEntity == null) return;
-        if (getRecipeMap() == null) return;
-        IMetaTileEntity aMetaTileEntity = aTileEntity.getMetaTileEntity();
-        if (aMetaTileEntity == null) return;
-        if (aMetaTileEntity instanceof MTEHatchInputBus)
-            ((IMultiblockRecipeMap) aMetaTileEntity).gtnl$setRecipeMapName(getRecipeMap().unlocalizedName);
+        gtnl$setRecipeMapNameIfValid(aTileEntity);
     }
 
     @Inject(method = "addInputHatchToMachineList", at = @At(value = "HEAD"))
     private void gtnl$InputHatchCheck(IGregTechTileEntity aTileEntity, int aBaseCasingIndex,
         CallbackInfoReturnable<Boolean> cir) {
+        gtnl$setRecipeMapNameIfValid(aTileEntity);
+    }
+
+    @Unique
+    private void gtnl$setRecipeMapNameIfValid(IGregTechTileEntity aTileEntity) {
         if (!MainConfig.machine.enableHatchInterfaceTerminalEnhance) return;
         if (aTileEntity == null) return;
         if (getRecipeMap() == null) return;
+
         IMetaTileEntity aMetaTileEntity = aTileEntity.getMetaTileEntity();
-        if (aMetaTileEntity == null) return;
-        if (aMetaTileEntity instanceof MTEHatchInput)
-            ((IMultiblockRecipeMap) aMetaTileEntity).gtnl$setRecipeMapName(getRecipeMap().unlocalizedName);
+        if (aMetaTileEntity instanceof IMultiblockRecipeMap recipeMap) {
+            recipeMap.gtnl$setRecipeMapName(getRecipeMap().unlocalizedName);
+        }
     }
 
 }
