d3342b98dff52b45ef16e7875b7bc911fe9426ea
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2026-04-01 10:08:34 +0800

    Update MixinBaseMetaTileEntityRenderer.java (#471)

--- src/main/java/com/science/gtnl/mixins/early/AprilFool/MixinBaseMetaTileEntityRenderer.java
+++ src/main/java/com/science/gtnl/mixins/early/AprilFool/MixinBaseMetaTileEntityRenderer.java
@@ -1,11 +1,9 @@
 package com.science.gtnl.mixins.early.AprilFool;
 
+import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.science.gtnl.common.render.tile.BallRenderer;
 
@@ -13,12 +11,11 @@ import gregtech.api.metatileentity.BaseMetaTileEntity;
 import gregtech.common.render.BaseMetaTileEntityRenderer;
 
 @Mixin(value = BaseMetaTileEntityRenderer.class, remap = false)
-public class MixinBaseMetaTileEntityRenderer {
+public abstract class MixinBaseMetaTileEntityRenderer extends TileEntitySpecialRenderer {
 
-    @Inject(method = "renderTileEntityAt", at = @At("HEAD"), cancellable = true)
-    private void onPostTick(TileEntity te, double x, double y, double z, float timeSinceLastTick, CallbackInfo ci) {
+    @Override
+    public void renderTileEntityAt(TileEntity te, double x, double y, double z, float timeSinceLastTick) {
         if (!(te instanceof BaseMetaTileEntity baseTE)) return;
         BallRenderer.renderTileEntity(baseTE.getMetaTileEntity(), x, y, z, timeSinceLastTick);
-        ci.cancel();
     }
 }
