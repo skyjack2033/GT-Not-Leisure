fe328a85367126d8a30889f33f02ab73ad1579eb
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2026-04-01 11:29:59 +0800

    Update MixinBaseMetaTileEntityRenderer.java (#472)

--- src/main/java/com/science/gtnl/mixins/early/AprilFool/MixinBaseMetaTileEntityRenderer.java
+++ src/main/java/com/science/gtnl/mixins/early/AprilFool/MixinBaseMetaTileEntityRenderer.java
@@ -8,6 +8,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import com.science.gtnl.common.render.tile.BallRenderer;
 
 import gregtech.api.metatileentity.BaseMetaTileEntity;
+import gregtech.api.metatileentity.implementations.MTEMultiBlockBase;
 import gregtech.common.render.BaseMetaTileEntityRenderer;
 
 @Mixin(value = BaseMetaTileEntityRenderer.class, remap = false)
@@ -16,6 +17,8 @@ public abstract class MixinBaseMetaTileEntityRenderer extends TileEntitySpecialR
     @Override
     public void renderTileEntityAt(TileEntity te, double x, double y, double z, float timeSinceLastTick) {
         if (!(te instanceof BaseMetaTileEntity baseTE)) return;
-        BallRenderer.renderTileEntity(baseTE.getMetaTileEntity(), x, y, z, timeSinceLastTick);
+        var metaTE = baseTE.getMetaTileEntity();
+        if (!(metaTE instanceof MTEMultiBlockBase)) return;
+        BallRenderer.renderTileEntity(metaTE, x, y, z, timeSinceLastTick);
     }
 }
