b073c36f38aaec94dad6ae8cf3c9b5c6e365969f
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2026-04-15 09:19:53 +0800

    Update PlayerDollRenderManagerClient.java (#496)

--- src/main/java/com/science/gtnl/common/render/PlayerDollRenderManagerClient.java
+++ src/main/java/com/science/gtnl/common/render/PlayerDollRenderManagerClient.java
@@ -2,7 +2,8 @@ package com.science.gtnl.common.render;
 
 import static com.science.gtnl.ScienceNotLeisure.RESOURCE_ROOT_ID;
 
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
 import java.io.BufferedReader;
