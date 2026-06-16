0d97285f0c6dc32efa59a7bda64abb3853a2d211
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2026-06-16 16:51:30 +0800

    Update CustomFluidHatchGui.java

--- src/main/java/com/science/gtnl/common/gui/modularui/CustomFluidHatchGui.java
+++ src/main/java/com/science/gtnl/common/gui/modularui/CustomFluidHatchGui.java
@@ -1,6 +1,11 @@
 package com.science.gtnl.common.gui.modularui;
 
 import com.cleanroommc.modularui.drawable.UITexture;
+import com.cleanroommc.modularui.screen.ModularPanel;
+import com.cleanroommc.modularui.utils.Alignment;
+import com.cleanroommc.modularui.value.sync.PanelSyncManager;
+import com.cleanroommc.modularui.widget.ParentWidget;
+import com.cleanroommc.modularui.widgets.layout.Flow;
 import com.science.gtnl.common.gui.GTNLMui2Textures;
 import com.science.gtnl.common.machine.hatch.CustomFluidHatch;
 
@@ -10,6 +15,20 @@ public class CustomFluidHatchGui extends MTEHatchBaseGui<CustomFluidHatch> {
 
     public CustomFluidHatchGui(CustomFluidHatch hatch) {
         super(hatch);
+    }
+
+    @Override
+    protected ParentWidget<?> createContentSection(ModularPanel panel, PanelSyncManager syncManager) {
+        Flow mainRow = Flow.row()
+            .coverChildren()
+            .childPadding(1)
+            .crossAxisAlignment(Alignment.CrossAxis.START);
+
+        mainRow.child(createScreen(panel, syncManager, machine.getFluidTank()));
+        mainRow.child(createIO(panel, syncManager, machine.getInputSlot(), machine.getOutputSlot()));
+        mainRow.childIf(supportsFluidFilterScreen(), () -> createFilterScreen(panel, syncManager));
+
+        return super.createContentSection(panel, syncManager).child(mainRow);
     }
 
     @Override
