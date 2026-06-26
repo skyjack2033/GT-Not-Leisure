fb182836df41193b037c701a11d72ecb0b57b84b
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2026-06-26 21:06:26 +0800

    修复输入总成的logo

--- src/main/java/com/science/gtnl/common/gui/modularui/DualInputHatchGui.java
+++ src/main/java/com/science/gtnl/common/gui/modularui/DualInputHatchGui.java
@@ -1,22 +1,26 @@
 package com.science.gtnl.common.gui.modularui;
 
+import com.cleanroommc.modularui.api.drawable.IDrawable;
 import com.cleanroommc.modularui.drawable.UITexture;
 import com.cleanroommc.modularui.factory.PosGuiData;
 import com.cleanroommc.modularui.screen.ModularPanel;
 import com.cleanroommc.modularui.screen.UISettings;
 import com.cleanroommc.modularui.value.sync.FluidSlotSyncHandler;
 import com.cleanroommc.modularui.value.sync.PanelSyncManager;
+import com.cleanroommc.modularui.widgets.layout.Grid;
 import com.cleanroommc.modularui.widgets.slot.FluidSlot;
 import com.cleanroommc.modularui.widgets.slot.ItemSlot;
-import com.cleanroommc.modularui.widgets.slot.ModularSlot;
 import com.science.gtnl.common.gui.GTNLMui2Textures;
 import com.science.gtnl.common.machine.hatch.DualInputHatch;
 
 import gregtech.api.modularui2.GTGuiTextures;
 import gregtech.api.modularui2.GTGuis;
 import gregtech.common.gui.modularui.hatch.base.MTEHatchBaseGui;
+import gregtech.common.modularui2.widget.builder.ItemSlotGridBuilder;
 
 public class DualInputHatchGui extends MTEHatchBaseGui<DualInputHatch> {
+
+    public static final String ITEM_SLOT_GROUP = "gtnl_dual_input_hatch_item_inv";
 
     public DualInputHatchGui(DualInputHatch hatch) {
         super(hatch);
@@ -32,33 +36,34 @@ public class DualInputHatchGui extends MTEHatchBaseGui<DualInputHatch> {
             .build();
 
         SlotLayout layout = SlotLayout.of(machine);
-        addItemSlots(panel, layout);
+        addItemSlots(panel, syncManager, layout);
         addFluidSlots(panel, layout);
         panel.child(createLogo());
         return panel;
     }
 
-    public void addItemSlots(ModularPanel panel, SlotLayout layout) {
-        for (int row = 0; row < layout.itemRows; row++) {
-            for (int col = 0; col < layout.itemColumns; col++) {
-                int slotIndex = row * layout.itemColumns + col;
-                if (slotIndex < machine.itemSlotAmount - 1) {
-                    panel.child(
-                        new ItemSlot().slot(new ModularSlot(machine.inventoryHandler, slotIndex))
-                            .background(GTGuiTextures.SLOT_ITEM_STANDARD)
-                            .pos(layout.centerX + col * 18 + 5, layout.centerY + row * 18));
-                }
-            }
-        }
+    public void addItemSlots(ModularPanel panel, PanelSyncManager syncManager, SlotLayout layout) {
+        Grid itemSlots = new ItemSlotGridBuilder(machine.inventoryHandler, syncManager)
+            .size(layout.itemColumns, layout.itemRows)
+            .slotGroupKey(ITEM_SLOT_GROUP)
+            .itemSlotSupplier(() -> new ItemSlot().background(GTGuiTextures.SLOT_ITEM_STANDARD))
+            .build();
+
+        panel.child(itemSlots.pos(layout.centerX + 5, layout.centerY));
     }
 
     public void addFluidSlots(ModularPanel panel, SlotLayout layout) {
         for (int i = 0; i < machine.getFluidTanksForGui().length; i++) {
             panel.child(
                 new FluidSlot().syncHandler(new FluidSlotSyncHandler(machine.getFluidTanksForGui()[i]))
-                    .background(GTGuiTextures.SLOT_FLUID_TANK)
                     .pos(layout.centerX + 18 * layout.itemColumns + 5, layout.centerY + i * 18));
         }
+    }
+
+    @Override
+    protected IDrawable.DrawableWidget createLogo() {
+        return new IDrawable.DrawableWidget(getLogoTexture()).size(SLOT_SIZE)
+            .pos(machine.getCircuitSlotX() - 1, machine.getCircuitSlotY() + SLOT_SIZE);
     }
 
     @Override
