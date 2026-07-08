8c1797c471d1d4c4d38797180f4f12d0efb8762b
Circulate233 <a18966009058@163.com>
2026-07-08 23:07:01 +0800

    修复NEI下单的一些问题

--- src/main/java/com/science/gtnl/mixins/late/randomComplement/MixinGuiCraftConfirm.java
+++ src/main/java/com/science/gtnl/mixins/late/randomComplement/MixinGuiCraftConfirm.java
@@ -1,19 +1,24 @@
 package com.science.gtnl.mixins.late.randomComplement;
 
+import appeng.client.gui.implementations.GuiCraftConfirm;
+import appeng.client.gui.widgets.GuiAeButton;
+import appeng.core.sync.AppEngPacket;
+import appeng.core.sync.network.NetworkHandler;
+import appeng.core.sync.packets.PacketSwitchGuis;
+import com.llamalad7.mixinextras.expression.Definition;import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.science.gtnl.ScienceNotLeisure;
+import com.science.gtnl.client.GTNLInputHandler;
+import com.science.gtnl.common.packet.ContainerRollBACK;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import com.science.gtnl.client.GTNLInputHandler;
-
-import appeng.client.gui.implementations.GuiCraftConfirm;
-import appeng.client.gui.widgets.GuiAeButton;
 
 @Mixin(value = GuiCraftConfirm.class, remap = false)
 public abstract class MixinGuiCraftConfirm {
@@ -24,20 +29,19 @@ public abstract class MixinGuiCraftConfirm {
     @Shadow
     private GuiAeButton startWithFollow;
 
-    // TODO: Restore this redirect if AE2 reintroduces switchToOriginalGUI.
-    // @Redirect(
-    // method = "actionPerformed",
-    // at = @At(
-    // value = "INVOKE",
-    // target = "Lappeng/client/gui/implementations/GuiCraftConfirm;switchToOriginalGUI()V"))
-    // public void onActionPerformed0(GuiCraftConfirm instance) {
-    // GuiScreen oldGui;
-    // if ((oldGui = GTNLInputHandler.LAST_GUI_SCREEN) != null) {
-    // ScienceNotLeisure.network.sendToServer(new ContainerRollBACK());
-    // return;
-    // }
-    // this.switchToOriginalGUI();
-    // }
+    @Definition(id = "instance", field = "Lappeng/core/sync/network/NetworkHandler;instance:Lappeng/core/sync/network/NetworkHandler;")
+    @Definition(id = "sendToServer", method = "Lappeng/core/sync/network/NetworkHandler;sendToServer(Lappeng/core/sync/AppEngPacket;)V")
+    @Definition(id = "PacketSwitchGuis", type = PacketSwitchGuis.class)
+    @Expression("instance.sendToServer(new PacketSwitchGuis())")
+    @WrapOperation(method = "actionPerformed", at = @At("MIXINEXTRAS:EXPRESSION"))
+    public void onActionPerformed0(NetworkHandler instance, AppEngPacket message, Operation<Void> original) {
+        GuiScreen oldGui;
+        if ((oldGui = GTNLInputHandler.LAST_GUI_SCREEN) != null) {
+            ScienceNotLeisure.network.sendToServer(new ContainerRollBACK());
+            return;
+        }
+        original.call(instance, message);
+    }
 
     @Inject(method = "actionPerformed", at = @At("HEAD"))
     public void onActionPerformed1(GuiButton btn, CallbackInfo ci) {
@@ -45,7 +49,7 @@ public abstract class MixinGuiCraftConfirm {
             GuiScreen oldGui;
             if ((oldGui = GTNLInputHandler.LAST_GUI_SCREEN) != null) {
                 GTNLInputHandler.DELAY_METHOD = () -> Minecraft.getMinecraft()
-                    .displayGuiScreen(oldGui);
+                                                               .displayGuiScreen(oldGui);
             }
         }
     }
